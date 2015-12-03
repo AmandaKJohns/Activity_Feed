3f705e5e530fa7e91ae0534bb52e005497988fbe
AmandaKJohns <amandajohns77@gmail.com>
2015-12-02 23:48:23 -0500

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
   has_many :statuses
+  validates :first_name, :last_name, presence: true
+  validates :profile_name, presence: true,
+                           uniqueness: true,
+                           format: {
+                            with: /a-zA-Z0-9_-/,
+                            message: 'Must be formatted correctly.'
+                           }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
